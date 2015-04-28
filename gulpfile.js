5f681bb84412cd6180a5762f612d63e7db9c235c
Anis ABID Amundi <anis.abid-ext@amundi.com>
2015-04-28 16:01:24 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -9,38 +9,35 @@ function isOnlyChange(event) {
 }
 
 var options = {
-    path: {
-        src: {
+    path : {
+        src:{
             dir: "src/",
             less: "src/**/*.less",
             js: "src/**/*.js"
         },
-        dist: {
-            dir: "app/assets/",
-            css: "app/assets/css/",
-            js: "app/assets/js/"
+        dist:{
+            dir: "app/assets/dist/",
+            css: "app/assets/dist/css/",
+            js: "app/assets/dist/js/"
         }
 
     }
 };
 
-gulp.task('styles', function () {
+gulp.task('styles', function() {
     // place code for your default task here
     gulp.src('./src/less/style.less') //path to your main less file
         .pipe(less(
-            paths
-    :
-    [path.join(__dirname, 'less', 'includes')]
-    ))
-    .
-    pipe(gulp.dest(options.path.dist.css)); // your output folder
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        ))
+        .pipe(gulp.dest(options.path.dist.css)); // your output folder
 });
 
 gulp.task('watch', function () {
     gulp.watch([
         options.path.src.less
-    ], function (event) {
-        if (isOnlyChange(event)) {
+    ], function(event) {
+        if(isOnlyChange(event)) {
             gulp.start('styles');
         } else {
             gulp.start('inject');
@@ -48,7 +45,7 @@ gulp.task('watch', function () {
     });
 });
 
-gulp.task('build', ['styles'], function () {
+gulp.task('build', ['styles'], function() {
     // place code for your default task here
     console.log("Build Ok");
 });
