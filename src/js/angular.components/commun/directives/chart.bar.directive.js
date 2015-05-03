e7e315f678d174fc30ccf7de40a58bff9e4c0106
ABID ANIS <anab.tn@gmail.com>
2015-05-03 03:06:12 +0200

    update chart bar

---
+++ src/js/angular.components/commun/directives/chart.bar.directive.js
@@ -0,0 +1,95 @@
+(function (angular, d3js, Obj) {
+  'use strict';
+
+  actApp.directive('amChartBar', ['$timeout', function ($timeout) {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: {
+        parameters: '=',
+        options: '='
+      },
+      template: '<div></div>',
+      link: function (scope, elem, attrs) {
+
+        $timeout(function () {
+          var settings = {
+            // These are the defaults.
+            backgroundColor: "white",
+            width: (attrs.width == undefined) ? 70 : attrs.width,
+            height: (attrs.height == undefined) ? 70 : attrs.height,
+            json: scope.parameters
+          };
+          var color = [],
+            results,
+            data = [],
+            chart,
+            bars,
+            margin = 100,
+            w = 8,
+            h = 70,
+            x, y,
+            xAxis, yAxis;
+
+          results = d3.map(settings.json);
+
+
+          var max = 0,
+            quotient = 0;
+          results.forEach(function (key, val) {
+            for (key in val) {
+              if (val.hasOwnProperty(key)) {
+                var result = {};
+                result.status = key;
+                result.val = val[key];
+                data.push(result);
+                max = Math.max(max, val[key]);
+                color.push(Obj.color[key]);
+              }
+            }
+          });
+
+          quotient = max / h;
+
+
+          chart = d3.select(elem[0]).append('svg')
+            .attr('class', 'chart')
+            .attr('width', 35)
+            .attr('height', h)
+            .append('g').attr('transform', 'translate(0, 100)');
+
+
+
+          chart.select('g')
+            .attr('transform', 'translate(0, 100)');
+
+
+          // Bars
+          bars = chart.append('g')
+            .attr('class', 'bars');
+
+          bars.selectAll('rect')
+            .data(data)
+            .enter().append('rect')
+            .attr("fill", function (d, i) {
+              console.log(color[i]);
+              return color[i];
+            })
+            .attr('x', function (d, i) {
+              return 8.5 * i;
+            })
+            .attr('y', function (d) {
+              return (h - margin) - (d.val / quotient) + .5
+            })
+            .attr('width', w)
+            .attr('height', function (d) {
+              return ( d.val / quotient )
+            })
+            .append('g');
+
+        });
+
+      }
+    };
+  }]);
+})(angular, d3, Obj);
