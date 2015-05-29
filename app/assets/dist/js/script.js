e973e88875489065c81733a510e80943588d32f4
Anis Abid <anis.abid-ext@amundi.com>
2015-05-29 16:29:52 +0200

    add mode edit

--- app/assets/dist/js/script.js
+++ app/assets/dist/js/script.js
@@ -163,114 +163,6 @@ var Obj = {
 (function () {
 
   angular.module('actApp')
-    .controller('MainController', ['$scope', '$state',
-      function($scope) {
-
-        $scope.main = {};
-
-      }]);
-})();
-
-'use strict';
-
-(function () {
-
-  angular.module('actApp')
-    .controller('CampaignController', ['$scope', '$state', '$window', 'CampaignListData', 'TestListData', 'CampaignDetailData', 'ActScrollbarConfig',
-      function ($scope, $state, $window, CampaignListData, TestListData, CampaignDetailData, ActScrollbarConfig) {
-
-        $scope.scrollbarConfig = ActScrollbarConfig;
-
-        $scope.campaign = {
-          items: CampaignListData,
-          currentItem: CampaignListData[1],
-          tests: TestListData,
-          detail: CampaignDetailData,
-          modeEdit: false
-        };
-        // Toggle Mode Edit
-        $scope.toggleModeEdit = function () {
-          $scope.campaign.modeEdit = !$scope.campaign.modeEdit;
-          console.log($scope.campaign.modeEdit);
-        };
-
-        $scope.globalHide = {
-          view: {
-            left: false,
-            right: false,
-            center: {
-              header: true
-            }
-          }
-        };
-        // Actions Toggle Sidebar
-        $scope.toggleViewLeftRight = function (direction) {
-          $scope.globalHide.view[direction] = !$scope.globalHide.view[direction];
-        };
-        // Actions Toggle Center Header
-        $scope.toggleViewCenterHeader = function () {
-          $scope.globalHide.view.center.header = !$scope.globalHide.view.center.header;
-        };
-
-        // Actions Tree
-        $scope.onRemove = function (scope) {
-          if ($window.confirm('Are you sure to remove ?')) {
-            scope.remove();
-          }
-        };
-
-        $scope.update = false;
-        /*$scope.onUpdate = function () {
-          $scope.update = !$scope.update;
-        };*/
-
-        $scope.onStatusChange = function (scope) {
-          var nodeData = scope.$parentNodeScope.$modelValue;
-
-          var nodeParent = scope.$parentNodeScope;
-
-          var nodesParent = scope.$parentNodesScope;
-
-          /*console.log(nodeData.nodes.length);
-          console.log(nodeData);
-
-          console.log(nodesParent.$modelValue);
-
-          console.log(nodeParent.$childNodesScope);*/
-        };
-
-        var getRootNodesScope = function() {
-          return angular.element(document.getElementById("tree-main")).scope();
-        };
-
-        $scope.onCollapseAll = function() {
-          var scope = getRootNodesScope();
-          scope.collapseAll();
-        };
-
-        $scope.onExpandAll = function() {
-          var scope = getRootNodesScope();
-          scope.expandAll();
-        };
-
-
-        $scope.onAddItem = function (scope) {
-          var nodeData = scope.$modelValue;
-          nodeData.nodes.push({
-            id: nodeData.id * 10 + nodeData.nodes.length,
-            title: nodeData.title + '.' + (nodeData.nodes.length + 1),
-            status: 0,
-            nodes: []
-          });
-        };
-
-      }]);
-})();
-'use strict';
-
-(function () {
-
-  angular.module('actApp')
     .factory('ApplicationFactory', ['$http', '$q', 'ActRest',
       function ($http, $q, ActRest) {
 
@@ -393,6 +285,118 @@ var Obj = {
       }]);
 
 })();
+'use strict';
+
+(function () {
+
+  angular.module('actApp')
+    .controller('CampaignController', ['$scope', '$state', '$window', 'CampaignListData', 'TestListData', 'CampaignDetailData', 'ActScrollbarConfig',
+      function ($scope, $state, $window, CampaignListData, TestListData, CampaignDetailData, ActScrollbarConfig) {
+
+        $scope.scrollbarConfig = ActScrollbarConfig;
+
+        $scope.campaign = {
+          items: CampaignListData,
+          currentItem: CampaignListData[1],
+          tests: TestListData,
+          detail: CampaignDetailData,
+          modeEdit: false
+        };
+        // Toggle Mode Edit
+        $scope.toggleModeEdit = function () {
+          $scope.campaign.modeEdit = !$scope.campaign.modeEdit;
+          console.log($scope.campaign.modeEdit);
+        };
+
+        $scope.globalHide = {
+          view: {
+            left: false,
+            right: false,
+            center: {
+              header: true
+            }
+          }
+        };
+        // Actions Toggle Sidebar
+        $scope.toggleViewLeftRight = function (direction) {
+          $scope.globalHide.view[direction] = !$scope.globalHide.view[direction];
+        };
+        // Actions Toggle Center Header
+        $scope.toggleViewCenterHeader = function () {
+          $scope.globalHide.view.center.header = !$scope.globalHide.view.center.header;
+        };
+
+        // Actions Tree
+        $scope.onRemove = function (scope) {
+          if ($window.confirm('Are you sure to remove ?')) {
+            scope.remove();
+          }
+        };
+
+        $scope.update = false;
+        /*$scope.onUpdate = function () {
+          $scope.update = !$scope.update;
+        };*/
+
+        $scope.onStatusChange = function (scope) {
+          var nodeData = scope.$parentNodeScope.$modelValue;
+
+          var nodeParent = scope.$parentNodeScope;
+
+          var nodesParent = scope.$parentNodesScope;
+
+          /*console.log(nodeData.nodes.length);
+          console.log(nodeData);
+
+          console.log(nodesParent.$modelValue);
+
+          console.log(nodeParent.$childNodesScope);*/
+        };
+
+        var getRootNodesScope = function() {
+          return angular.element(document.getElementById("tree-main")).scope();
+        };
+
+        $scope.onCollapseAll = function() {
+          var scope = getRootNodesScope();
+          scope.collapseAll();
+        };
+
+        $scope.onExpandAll = function() {
+          var scope = getRootNodesScope();
+          scope.expandAll();
+        };
+
+
+        $scope.onAddItem = function (scope) {
+          var nodeData = scope.$modelValue;
+          nodeData.nodes.push({
+            id: nodeData.id * 10 + nodeData.nodes.length,
+            title: nodeData.title + '.' + (nodeData.nodes.length + 1),
+            status: 0,
+            nodes: []
+          });
+        };
+
+      }]);
+})();
+'use strict';
+
+(function () {
+
+  angular.module('actApp')
+    .controller('DashboardController', ['$scope', '$state', 'CampaignListData', 'ApplicationListData',
+      function ($scope, $state, CampaignListData, ApplicationListData) {
+
+        $scope.dashboard = {
+          sources: CampaignListData,
+          applications: ApplicationListData
+        };
+
+      }]);
+})();
+
+
 
 'use strict';
 
@@ -723,15 +727,11 @@ var Obj = {
 (function () {
 
   angular.module('actApp')
-    .controller('DashboardController', ['$scope', '$state', 'CampaignListData', 'ApplicationListData',
-      function ($scope, $state, CampaignListData, ApplicationListData) {
+    .controller('MainController', ['$scope', '$state',
+      function($scope) {
 
-        $scope.dashboard = {
-          sources: CampaignListData,
-          applications: ApplicationListData
-        };
+        $scope.main = {};
 
       }]);
 })();
-
 
